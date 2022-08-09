cacfebf27d43ea8e7e4b4dffba578adda6a58653
Gabriel Seals <52087296+gseals@users.noreply.github.com>
2022-08-09 17:34:02 -0500

    added heading (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
+// Conventional props
+function Heading({ title }: { title: string }) {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
 function App() {
   return (
     <div>
-
+      <Heading title="Hello there" />
     </div>
   );
 }
